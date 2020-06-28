443a1b30c708e612e021070fd9f0a04c98b4cf57
AppVeyor <ci@appveyor.com>
2020-06-28 16:39:56 +0000

    5550fb1

---
+++ precache-manifest.faafb9c586ec85ac266faedeb71b6c17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca2a00b74dfae650ea65b190204e15f3",
+    "url": "/world_clocks/index.html"
+  },
+  {
+    "revision": "a24829ee788d2867fd8a",
+    "url": "/world_clocks/static/css/main.885d1e2a.chunk.css"
+  },
+  {
+    "revision": "cd8e55077afbd26b138c",
+    "url": "/world_clocks/static/js/2.15e74a71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/world_clocks/static/js/2.15e74a71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a24829ee788d2867fd8a",
+    "url": "/world_clocks/static/js/main.228bdb17.chunk.js"
+  },
+  {
+    "revision": "495731bc349d427fc0be",
+    "url": "/world_clocks/static/js/runtime-main.3dbcb770.js"
+  }
+]);
